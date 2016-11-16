e0904d844a0bc25ecd6df4a1ea7693a2ebc26464
Grasseh <steve.gagnev4si@gmail.com>
2016-11-16 10:43:37 -0500

    Updated Test suite for problems

--- src/adventofcode2016/problems/day0a.clj
+++ src/adventofcode2016/problems/day0a.clj
@@ -1,7 +1,11 @@
 (ns adventofcode2016.problems.day0a)
 
+(defn doStuff [filename]
+  1)
 
 (defn solve
   "Return the third input in the provided file"
   [filename]
-  (println (str "Your solution is " filename)))
+  (let [solution (doStuff filename)]
+    (println (str "Your solution is " solution))
+    solution))
